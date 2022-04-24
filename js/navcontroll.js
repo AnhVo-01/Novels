7114684a9fcde78abefda7a020a0627b3ff465ac
Avin-Vo <anhvo.1305@gmail.com>
2022-04-24 21:30:09 +0700

    create home page

--- js/navcontroll.js
+++ js/navcontroll.js
@@ -2,21 +2,23 @@ const opt = document.querySelectorAll(".dropdown"),
     optShow = document.querySelectorAll(".dropdown-menu");
 
 for (let i = 0; i < opt.length; i++){
-    opt[i].onclick = () =>{
+    opt[i].addEventListener("click", function() {
         for (let j = 0; j < opt.length; j++){
             if(opt[j].classList.contains("open")){
                 opt[j].classList.remove("open");
             }
         }
         opt[i].classList.toggle("open");
-    }
+    });  
 }
-
-// // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    for (let k = 0; k < opt.length; k++){
-        if (event.target == opt[k]) {
-            opt[k].classList.remove("open");
+  
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+    if (!e.target.matches('.text')) {
+        for(let i = 0; i < opt.length; i++){
+            if (opt[i].classList.contains('open')) {
+                opt[i].classList.remove('open');
+            }
         }
     }
 }
